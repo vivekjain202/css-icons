cbc02449d489d294f037efe41be61508fc5b242e
Vivek Jain <37824719+vivekjain202@users.noreply.github.com>
2023-03-13 04:38:51 +0000

    Added watermelon slice solution

--- src/components/single-element-icons/watermelonSlice.js
+++ src/components/single-element-icons/watermelonSlice.js
@@ -41,6 +41,41 @@ export const WatermelonSlice = {
     name: 'Watermelon slice',
     id: 'watermelon_slice',
     component: WatermelonSliceIcon,
-    html:``,
-    css: ``
+    html:`<div id='watermelonSlice'></div>`,
+    css: `
+        #watermelonSlice{
+            width: 50px;
+            height: 25px;
+            background: black;
+            border-bottom-left-radius: 50px;
+            border-bottom-right-radius: 50px;
+            position: relative;
+            transform: rotate(-30deg);
+        }
+        #watermelonSlice::before {
+            content: '';
+            position: absolute;
+            width: 60px;
+            height: 30px;
+            border-bottom-left-radius: 60px;
+            border-bottom-right-radius: 60px;
+            top: 0px;
+            left: -6px;
+            border: solid black;
+            border-top: 0px;
+            background: transparent;
+        }
+        #watermelonSlice::after {
+            content: '';
+            position: absolute;
+            width: 5px;
+            height: 8px;
+            border-radius: 2px;
+            background: white;
+            top: 2px;
+            left: 5px;
+            box-shadow: 9px 7px 0 0 white, 17px 0 0 0 white, 
+                        25px 7px 0 0 white, 35px 0 0 0 white;
+        }
+    `
 }
